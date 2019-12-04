f7bf8a984395b575f43037ddde4d23d39fb6adb4
Scott_Mac <scott_mac@DN525en2.SUNet>
2019-12-04 11:57:26 -0800

    multicore

--- Expectmax_Opt/puzzle.py
+++ Expectmax_Opt/puzzle.py
@@ -5,14 +5,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 import ExpectiMax
 import logic
+import multiprocessing
 
-ITERATION=10
+ITERATION=100
 DEPTH=4
-stat=[]
 
-def main():
-    for iteration in range(ITERATION):
-        print("Iteration: "+str(iteration+1))
+def iteration(iter):
         start=time.time()
         step=0
         matrix=np.zeros((4,4),dtype=np.int)
@@ -26,13 +24,22 @@ def main():
             matrix=ExpectiMax.moveGrid(matrix,move)
             step+=1
 
+        print("Iteration: "+str(iter+1))
         print("Step= "+str(step))
         print("Max= "+str(2**np.max(matrix)))
         print("Score= "+str(logic.getScore(matrix)))
         print('Depth= '+str(DEPTH))
         print('Time= '+str(time.time()-start))
         print('')
-        stat.append((step,2**np.max(matrix),logic.getScore(matrix)))
+
+        return (step,2**np.max(matrix),logic.getScore(matrix))
+
+
+def main():
+    p=multiprocessing.Pool(processes=ITERATION)
+    data=p.map(iteration,[i for i in range(ITERATION)])
+    p.close()
+    np.savetxt('stat.txt',data)
 
 if __name__ == '__main__':
     main()
